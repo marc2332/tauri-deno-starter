ad7eacbb95564b075bb44df1f2afc955d19ee783
Daniel Mizerski <danielduel-github@dduel.dev>
2024-01-04 18:11:48 +0100

    feat: update bundle to latest esbuild and use live-reload

--- www/index.tsx
+++ www/index.tsx
@@ -1,6 +1,15 @@
-import React from "https://esm.sh/react@18" 
-import { createRoot } from "https://esm.sh/react-dom@18/client" 
+declare const __USE_LIVE_RELOAD: boolean | undefined;
+
+import React from "https://esm.sh/react@18";
+import { createRoot } from "https://esm.sh/react-dom@18/client";
 import App from  "./App.tsx";
+
+// __USE_LIVE_RELOAD is defined in `define` and gets replaced by esbuild
+const useLiveReload = __USE_LIVE_RELOAD;
+if (useLiveReload) {
+  console.log("Using live reload")
+  new EventSource('/esbuild').addEventListener('change', () => location.reload());
+}
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
